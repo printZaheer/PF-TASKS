31ff4e90f9c97a2eabfbb34e56f2b082bc961215
printZaheer <232248698+printZaheer@users.noreply.github.com>
2025-10-20 23:54:54 +0500

    Diamond shape formed using Nested for loops

---
+++ DiamondShape_Pattern.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 5; 
+
+    
+    for (int i = 1; i <= n; i++)
+    {
+        
+        for (int s = i; s < n; s++)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    
+    for (int i = n - 1; i >= 1; i--)
+    {
+        
+        for (int s = n; s > i; s--)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

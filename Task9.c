92c4f97c31e99dc5d0573f32882fe2556bab1b25
printZaheer <232248698+printZaheer@users.noreply.github.com>
2025-10-21 23:25:36 +0500

    The task of printing streric pattern

---
+++ Task9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    for (int x = 1; x <= 5; x++)
+    {
+        for (int y = 1; y <= x; y++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+    for (int x = 1; x <= 5; x++)
+    {
+        for (int y = 5; y >= x; y--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

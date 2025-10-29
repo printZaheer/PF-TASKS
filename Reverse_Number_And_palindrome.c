475a15e9ae6504d8147746e166e6bbbf02f67fe6
printZaheer <232248698+printZaheer@users.noreply.github.com>
2025-10-29 23:53:11 +0500

    Modify in Reverse number and its output

--- Reverse_Number_And_palindrome.c
+++ Reverse_Number_And_palindrome.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
-int main(){
-int num,original_num,reverse,rem;
-reverse=0;
-printf("Enter the num: ");
-scanf("%d",&num);
+int main()
+{
+    int num, original_num, reverse, rem;
+    reverse = 0;
+    printf("Enter the num: ");
+    scanf("%d", &num);
 
-original_num=num;
+    original_num = num;
 
-while(num!=0){
-rem=num%10;
-reverse=reverse*10+rem;
-num=num/10;
+    while (num != 0)
+    {
+        rem = num % 10;
+        reverse = reverse * 10 + rem;
+        num = num / 10;
+    }
+    printf("reverse number is = %d\n", reverse);
 
-}
-printf("reverse number is = %d\n",reverse);
-
-if (original_num==reverse)
-    printf("Palindrome");
-else
-    printf("Not Palindrome");
+    if (original_num == reverse)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
 
     return 0;
 }
